899a853fad6b719790100e31a47477eafc3f31f0
nicodeas <nicholas.hy.michlin@gmail.com>
2023-08-11 18:59:51 +0800

    format files

--- server/app.py
+++ server/app.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-import logging
-logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
-
-
-
+    return render_template("index.html")
